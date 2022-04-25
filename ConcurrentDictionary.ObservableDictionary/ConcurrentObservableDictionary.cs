cc17a13e0299ae57466f22f5fecd84b7e016cdbd
Pratap Singh <psingh@nymi.com>
2022-04-25 19:48:03 +0530

    Incorported event args intosingle file

--- ConcurrentDictionary.ObservableDictionary/ConcurrentObservableDictionary.cs
+++ ConcurrentDictionary.ObservableDictionary/ConcurrentObservableDictionary.cs
@@ -135,4 +135,17 @@ namespace ConcurrentDictionary.ObservableDictionary
             }            
         }
     }
+    public class TimeOutEventArgs : EventArgs
+    {
+        private string action;
+        private object item;
+
+        public TimeOutEventArgs(string Action, object DictItem)
+        {
+            this.action = Action;
+            this.item = DictItem;
+        }
+        public string Action { get { return action; } }
+        public object Item { get { return item; } }
+    }
 }
